6b40fe9dfed4951d53c0d46c2a13d1ae1186f95e
StarlightQueen21 <43399723+StarlightQueen21@users.noreply.github.com>
2018-11-14 09:19:52 +0100

    yay
    yay2

---
+++ src/components/footer.js
@@ -0,0 +1,6 @@
+import React from "react"; //že chceme teda psát ten react
+import ReactDOM from "react-dom"; //document object model
+
+function Foo() {
+  return <h3>Ahoj</h3>;
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from "react"; //že chceme teda psát ten react
 import ReactDOM from "react-dom"; //document object model
-
+import Footer from "./components/footer";
 import "./styles.css";
 
 function App() {
   //nejake funkce, tahle nám dělá tu applikaci, vrací to html
 
+  function aktualniDEn() {
+    var today = new Date();
+    return today.getDate();
+  }
+
+  const den = "středa";
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      NOPE NOPE NOPE ><br />
+      NOPE NOPE NOPE <br />
       no f*cking magic
+      <h2>
+        Dnes je {den} nebo spíš {aktualniDEn()}
+      </h2>
     </div>
   );
 }
